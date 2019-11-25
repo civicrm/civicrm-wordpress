190bf5daf43e20c7f4e8866c2f5c4056cc6aa729
Christian Wach <needle@haystack.co.uk>
2019-11-25 09:07:49 +0000

    Remove debugging code

--- civicrm.php
+++ civicrm.php
@@ -370,14 +370,6 @@ class CiviCRM_For_WordPress {
       $civicrm_paths['cms.root']['path'] = untrailingslashit(ABSPATH);
     }
 
-    $e = new Exception;
-    $trace = $e->getTraceAsString();
-    error_log( print_r( array(
-      'method' => __METHOD__,
-      'civicrm_paths' => $civicrm_paths,
-      'backtrace' => $trace,
-    ), true ) );
-
     // Get classes and instantiate
     $this->include_files();
 
