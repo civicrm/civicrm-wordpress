44842ac56240fe79877f766d139067232684ccf0
Tim Otten <totten@civicrm.org>
2021-02-17 20:58:51 -0800

    wp-cli - Add autogenerated signing keys for JWT

--- wp-cli/civicrm.php
+++ wp-cli/civicrm.php
@@ -456,6 +456,7 @@ if (!defined('CIVICRM_WPCLI_LOADED')) {
         'CMSdbName'          => DB_NAME,
         'siteKey'            => preg_replace(';[^a-zA-Z0-9];', '', base64_encode(random_bytes(37))),
         'credKeys'           => 'aes-cbc:hkdf-sha256:' . preg_replace(';[^a-zA-Z0-9];', '', base64_encode(random_bytes(37))),
+        'signKeys'           => 'jwt-hs256:hkdf-sha256:' . preg_replace(';[^a-zA-Z0-9];', '', base64_encode(random_bytes(37))),
       ];
 
       $str = file_get_contents($tplPath . 'civicrm.settings.php.template');
